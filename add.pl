3e251135c6c818b9b0b3925e4b461a94be87c86d
root <root@tonju01-usca02.(none)>
2014-08-14 16:37:37 -0700

    prettify output
    check_typo now will go on to next ip if it encounters a problem
    also will display more helpful output than just declare that it
    is wrong
    
    now diff output is checked, if there are no differences, dont
    bother asking user to accept changes (no changes to accept)
    
    if the /tmp file counterpart is found, slurp that instead of the
    old one in /etc/bind/zone (applies for both add_fwddns_entry and
    add_revdns_entry

--- add.pl
+++ add.pl
@@ -19,9 +19,19 @@ sub main {
 	my %hash_of_updated_files;
 	my @processed_ips;
 
+	#clean up any temp files from prev runs
+	system("rm -rf /tmp/db.*");
+
 	foreach my $ip (@ARGV) {
 
-		&check_typo($ip); #makes sure input ip is correct	format
+		print "------------------\n";
+
+		if(&check_typo($ip)){ #makes sure input ip is correct	format
+			#true means it is ok
+		}else{
+			next;
+		}
+
 		#check that it exists in our database
 		if(!exists($$newips{$ip})){
 			print "unknown $ip, update __DATA__ section to include this entry\n";
@@ -46,26 +56,31 @@ sub main {
 
 	if(scalar @processed_ips > 0){
 
+		print "==================\n";
+
 		#db.paraccel.com is always updated so we add this entry
 		$hash_of_updated_files{'db.paraccel.com'}=0;
 
 		#run setserial on updated db.* files
-		foreach my $file (keys %hash_of_updated_files){
+		foreach my $file (sort keys %hash_of_updated_files){
 
 			#do a diff of the changes and check with user before overwriting
 			my $command = "diff /tmp/$file /etc/bind/zones/$file";
 			print "running $command\n";
-			system($command);
+			my $rv = system($command);
 
+			if($rv != 0){
 			print "Accept changes? (y/n) ";
 			my $answer=<STDIN>;
 			chomp($answer);
 			if($answer eq 'y'){
 				print "overwriting $file\n";
 				system("mv /tmp/$file /etc/bind/zones/$file");
+				system("chmod a+x /etc/bind/zones/$file");
 
 				print "running setserial $file\n";
 				system("setserial $file");
+			}
 			}else{
 				next;
 			}
@@ -81,7 +96,7 @@ sub main {
 		foreach my $ip (@processed_ips){
 			my $hostname = $$newips{$ip};
 			$hostname =~ s/\.paraccel\.com//;
-			print "ping -c 1 $hostname\n";
+			print "ping -c1 -w1 $hostname\n";
 			system("ping -c1 -w1 $hostname");
 		}
 	}
@@ -97,7 +112,15 @@ sub add_forwarddns_entry{
 	my $ip = shift;
 	my $fqdn = shift;
 
-	my $file_contents = &slurp("./db.paraccel.com");
+	my $file_contents;
+
+	#need to give preference to the file in /tmp as this will
+	#have the latest changes
+	if(-e "/tmp/db.paraccel.com"){
+		$file_contents = &slurp("/tmp/db.paraccel.com");
+	}else{
+		$file_contents = &slurp("./db.paraccel.com");
+	}
 	my @file_contents = split(/\n/,$file_contents);
 
 	$fqdn =~ /(.*)\.paraccel\.com/;
@@ -106,18 +129,18 @@ sub add_forwarddns_entry{
 	my $line_number=0;
 	foreach my $line (@file_contents){
 		if($line =~ /^$hostname\s/){
-			print "found:\n$line\n";
+			#print "found:\n$line\n";
 			$line =~ s/\d+\.\d+\.\d+\.\d+/$ip/;
 			$file_contents[$line_number] = $line;
 
-			print "updated to:\n$file_contents[$line_number]\n";
+			#print "updated to:\n$file_contents[$line_number]\n";
 
 		}
 		$line_number++;
 	}
 
 	#ready to output @file_contents back into a file
-	print "-----------------\n";
+	#print "-----------------\n";
 	open(OUTPUT,'>',"/tmp/db.paraccel.com") or die $!;
 
 	foreach my $line (@file_contents){
@@ -135,7 +158,15 @@ sub add_reversedns_entry{
 	my $file_to_open = "db.$octet[-2].$octet[-3].$octet[-4]";
 	#print "$file_to_open\n"; #check to see if the filename is setup correctly
 
-	my $file_contents = &slurp("./$file_to_open");
+	my $file_contents;
+
+	#need to give preference to the file in /tmp as this will
+	#have the latest changes
+	if(-e "/tmp/$file_to_open"){
+		$file_contents = &slurp("/tmp/$file_to_open");
+	}else{
+		$file_contents = &slurp("./$file_to_open");
+	}
 	my @file_contents = split(/\n/,$file_contents);
 	my $line_number=0;
 	my $doitonce=1;
@@ -144,7 +175,11 @@ sub add_reversedns_entry{
 	foreach my $line (@file_contents){
 		if($line =~ /^(\d+)/){
 			if($1 == $octet[-1]){
-				print "$line_number: entry already exists!: $line\n";
+				#print "$line_number: entry already exists!: $line\n";
+				print "entry already exists!: $line\n";
+				if(! -e "/tmp/$file_to_open"){
+					system("cp /etc/bind/zones/$file_to_open /tmp");
+				}
 				return;
 			}elsif(($1 > $octet[-1]) && $doitonce){
 				#print "$line_number: $line\n";
@@ -169,7 +204,8 @@ sub add_reversedns_entry{
 	splice @file_contents, $splice_here, 0, "$octet[-1]\t\tPTR\t$hostname.";
 
 	#ready to output @file_contents back into a file
-	print "-----------------\n";
+	#print "-----------------\n";
+	#print "creating /tmp/$file_to_open\n";
 	open(OUTPUT,'>',"/tmp/$file_to_open") or die $!;
 	foreach my $line (@file_contents){
 		print OUTPUT "$line\n";
@@ -182,10 +218,14 @@ sub add_reversedns_entry{
 sub check_typo{
 	my $ip = shift @_;
 	if($ip !~ /10\.11\.(10|11|13|30|31|32|33|250)\.\d+$/){
-		die "typo detected: $ip\n";
+		print "typo detected: $ip\n";
+		print "ips need to start with 10.11.{10|11|13|30|31|32|33|250}.xxx\n";
+		print "example: add.pl 10.11.30.14\n";
+		return 0;
+	}else{
+		return 1;
 	}
 }
-
 sub print_hash {
 	my $hash=shift @_;
 	foreach my $key (keys %$hash){
